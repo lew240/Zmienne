539c170588e9505aeb51adad134e48588d815941
Michal <lew240@wp.pl>
2022-10-16 20:03:38 +0200

    pierwszy commit

---
+++ src/s1_zmienne/Zmienneintro.java
@@ -0,0 +1,11 @@
+package s1_zmienne;
+
+
+public class Zmienneintro {
+    public static void main (String [] args ){
+        int mojWiek = 18;
+        System.out.println("Mam " + mojWiek + " lat");
+        int twojWiek = 20;
+        System.out.println("Ty masz " + twojWiek + "lat.");
+    }
+}
